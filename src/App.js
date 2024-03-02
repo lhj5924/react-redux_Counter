d21f553b96ad6332cd4efb62ce0d877d511d83b6
Hyun Jin, Lee <nagi5924@gmail.com>
2024-03-02 23:12:40 +0900

    [Docs] Add: README.md updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement } from "./actions/index"
+import styled from "styled-components"
 
 function App() {
+  const count = useSelector((state) => state.count) // useSelector를 사용하여 Redux 스토어의 상태를 선택합니다.
+  const dispatch = useDispatch() // useDispatch를 사용하여 액션을 디스패치합니다.
+  console.log(count) // 왜 count 의 초기값이 counterReducer.js 에 정의한 0이 아니라 undefined 로 나오는거지?
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <Container>
+        <Counter>
+          Counter: <Count>{count}</Count>
+        </Counter>
+        <Button onClick={() => dispatch(increment())}>Increment</Button>{" "}
+        {/* Increment 액션을 디스패치하는 버튼 */}
+        <Button onClick={() => dispatch(decrement())}>Decrement</Button>{" "}
+        {/* Decrement 액션을 디스패치하는 버튼 */}
+        <P>Please check the console</P>
+      </Container>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
+
+//==================== Styled Components ====================//
+
+const AppContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Container = styled.div`
+  width: 560px;
+  background-color: wheat;
+`
+const Counter = styled.h1`
+  color: darkcyan;
+`
+const Count = styled.span`
+  color: darkblue;
+  font-weight: 600;
+`
+const Button = styled.button`
+  border: none;
+  background-color: darkcyan;
+  color: white;
+  padding: 1rem;
+  &:hover {
+    cursor: pointer;
+    background-color: darkblue;
+  }
+`
+const P = styled.p`
+  color: darkcyan;
+`
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js (또는 최상위 컴포넌트)
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import store from "./store/store" // 위에서 생성한 스토어 가져오기
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  document.getElementById("root")
+)
