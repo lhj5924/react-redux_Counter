a94891b1924f0c3069b0c44ead9f58c865d5ecab
Hyun Jin, Lee <nagi5924@gmail.com>
2024-03-02 23:27:22 +0900

    [Change] store.js : discard rootReducer.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 // store.js
 import { createStore } from "redux"
-import rootReducer from "../reducer/rootReducer" // 루트 리듀서 가져오기
+// import rootReducer from "../reducer/rootReducer" // 루트 리듀서 가져오기
+import counterReducer from "../reducer/counterReducer" // 카운터 리듀서만 가져오기
 
-const store = createStore(rootReducer)
+const store = createStore(counterReducer)
 export default store
